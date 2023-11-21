7ae5499ffaaac2bd58a04755d969acbcf28c1843
Tymur Kosiak <us1gav@gmail.com>
2023-11-21 13:35:46 +0200

    Homework lesson28 create.

---
+++ src/main/java/edu/hillel/springhwlesson28/SpringHwLesson28Application.java
@@ -0,0 +1,13 @@
+package edu.hillel.springhwlesson28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHwLesson28Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHwLesson28Application.class, args);
+    }
+
+}
